589594a2a477a39e2738298e41a4b201fe0d41dc
nanakomatasu <123913335+nanakomatasu@users.noreply.github.com>
2023-02-05 20:23:23 +0800

    完成作业

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const fs = require("fs");
-const res = fs.readFileSync("./info.json", "utf8");
-const arr = JSON.parse(res);
+const path = require("path");
+const file = path.join(__dirname, "/info.json");
+const arr = JSON.parse(fs.readFileSync(file, "utf-8"));
 console.log(arr);
 const item = arr[Math.floor(Math.random() * arr.length)];
 console.log(item);
-
-// const socres = arr.map((item) => {
-//   const newArr = item.split("=");
-//   return +newArr[1];
-// });
-
-// const total = socres.reduce((a, s) => {
-//   return a + s;
-// }, 0);
-// console.log(total);
-
-// fs.writeFileSync("./成绩.txt", total + "");
+console.log(arr.length);
+const id = Math.floor(Math.random() * arr.length + 1);
+console.log(id);
+arr[id - 1].count = arr[id - 1].count + 1;
+fs.writeFileSync(file, JSON.stringify(arr));
